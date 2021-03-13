26bb45d00e997462986cdf56d6ec397871dcc909
appworld4u2017 <32671008appworld>
2021-03-13 11:04:46 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+/*
+var thePets = [
+    {
+        "name":"Cat1",
+        "species":"cat",
+        "favFood":"tuna"
+    },
+    {
+        "name":"Dog1",
+        "species":"dog",
+        "favFood":"biscuit"
+    }
+];
+
+thePets[1].species;
+
+*/
+
+var pageCounter = 1;
+
+var animalContainer = document.getElementById("results");
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+
+var ourRequest = new XMLHttpRequest();
+
+ourRequest.open("GET","https://learnwebcode.github.io/json-example/animals-"+pageCounter+".json");
+
+ourRequest.onload = function(){
+var ourData = JSON.parse(ourRequest.responseText);
+
+renderHtml(ourData);
+
+};
+ourRequest.send();
+pageCounter++;
+if(pageCounter>3){
+    btn.classList.add("hide-me");
+}
+});
+
+
+function renderHtml(data){
+var htmlString = "";
+
+for(i=0; i<data.length; i++){
+    htmlString += "<p>"+data[i].name + " is a "+data[i].species + "</p>"
+}
+
+animalContainer.insertAdjacentHTML('beforeend',htmlString);
+
+
+}
+
+
+
+
+
